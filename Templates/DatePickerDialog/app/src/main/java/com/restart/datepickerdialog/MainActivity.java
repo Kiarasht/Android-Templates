1a739786c92e6a37ac650236fa4f67c1b8a72dac
Kiarasht <kiarashbikar@gmail.com>
2017-05-28 15:23:00 -0700

    files for it woops

---
+++ Templates/DatePickerDialog/app/src/main/java/com/restart/datepickerdialog/MainActivity.java
@@ -0,0 +1,65 @@
+package com.restart.datepickerdialog;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private SimpleDateFormat mSimpleDateFormat;
+    private Calendar mCalendar;
+    private Activity mActivity;
+    private TextView mDate;
+
+    /* Set up view, variables, and OnClickListener */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mActivity = this;
+        mSimpleDateFormat = new SimpleDateFormat("MM/dd/yy h:mm a", Locale.getDefault());
+        mDate = (TextView) findViewById(R.id.contentMain);
+        mDate.setOnClickListener(textListener);
+    }
+
+    /* Define the onClickListener, and start the DatePickerDialog with users current time */
+    private final View.OnClickListener textListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mCalendar = Calendar.getInstance();
+            new DatePickerDialog(mActivity, mDateDataSet, mCalendar.get(Calendar.YEAR),
+                    mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    };
+
+    /* After user decided on a date, store those in our calendar variable and then start the TimePickerDialog immediately */
+    private final DatePickerDialog.OnDateSetListener mDateDataSet = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mCalendar.set(Calendar.YEAR, year);
+            mCalendar.set(Calendar.MONTH, monthOfYear);
+            mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            new TimePickerDialog(mActivity, mTimeDataSet, mCalendar.get(Calendar.HOUR), mCalendar.get(Calendar.MINUTE), false).show();
+        }
+    };
+
+    /* After user decided on a time, save them into our calendar instance, and now parse what our calendar has into the TextView */
+    private final TimePickerDialog.OnTimeSetListener mTimeDataSet = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            mCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            mCalendar.set(Calendar.MINUTE, minute);
+            mDate.setText(mSimpleDateFormat.format(mCalendar.getTime()));
+        }
+    };
+}
