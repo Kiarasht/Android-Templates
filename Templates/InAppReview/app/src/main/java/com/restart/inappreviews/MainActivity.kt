09e02a2c28aba08b61b84616ac396f36a0c84c8b
Kiarash Torkian <kiarash.torkian@nike.com>
2020-12-31 23:36:23 -0800

    In app review finalized.
    https://play.google.com/apps/test/RQJmzVBRx9k/ahAEJTaaQtDaJlQyj2iQTYb4ekJLwdTXABfcx89yT3quiPtaK5W07YkExpc3Q0uZxHgAX03k_3jZsiJJJjR_i5EyC3

--- Templates/InAppReview/app/src/main/java/com/restart/inappreviews/MainActivity.kt
+++ Templates/InAppReview/app/src/main/java/com/restart/inappreviews/MainActivity.kt
@@ -2,36 +2,34 @@ package com.restart.inappreviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import com.google.android.play.core.review.ReviewManagerFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val manager = ReviewManagerFactory.create(this)
+        launch_review.setOnClickListener {
+            val manager = ReviewManagerFactory.create(this)
 
-        val request = manager.requestReviewFlow()
-        request.addOnCompleteListener { request ->
-            if (request.isSuccessful) {
-                Log.d("Review", "Got request")
-                // We got the ReviewInfo object
-                val reviewInfo = request.result
+            val reviewFlow = manager.requestReviewFlow()
+            reviewFlow.addOnCompleteListener { request ->
+                if (request.isSuccessful) {
+                    // We got the ReviewInfo object
+                    val reviewInfo = request.result
 
-                val flow = manager.launchReviewFlow(this, reviewInfo)
-                flow.addOnCompleteListener {
-                    Log.d("Review", "Finished")
-                    // The flow has finished. The API does not indicate whether the user
-                    // reviewed or not, or even whether the review dialog was shown. Thus, no
-                    // matter the result, we continue our app flow.
+                    val flow = manager.launchReviewFlow(this, reviewInfo)
+                    flow.addOnCompleteListener {
+                        // The flow has finished. The API does not indicate whether the user
+                        // reviewed or not, or even whether the review dialog was shown. Thus, no
+                        // matter the result, we continue our app flow.
+                    }
+                } else {
+                    Toast.makeText(this, "Failed in requesting review flow", Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                Log.d("Review", "Error")
-                // There was some problem, continue regardless of the result.
             }
         }
-
-
     }
 }
